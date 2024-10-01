95bb80d526d6d7e8dbdc8b6778d31c85f98e849c
raikiri ^^ <143789866+Naad0427@users.noreply.github.com>
2024-10-01 18:27:21 +0800

    Update Activity_AldayaNaad.java
    Hello Yes

--- Activity_AldayaNaad/src/activity_aldayanaad/Activity_AldayaNaad.java
+++ Activity_AldayaNaad/src/activity_aldayanaad/Activity_AldayaNaad.java
@@ -10,11 +10,10 @@ package activity_aldayanaad;
  */
 public class Activity_AldayaNaad {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
+    }
     }
     
 }
